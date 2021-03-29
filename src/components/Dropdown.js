d2854e381739b0988bbc031b8a288905f805e72d
atotared <atota@red.software.systems>
2021-03-29 23:10:00 +0200

    Implement skeleton of Dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 const Dropdown = ({ options }) => {
-  const dropdownItems = options.map((item) => {
+  const dropdownOptions = options.map((item) => {
     return (
       <option key={item.color} value={item.color}>
         {item.label}
       </option>
     );
   });
-  return <select className="ui dropdown">{dropdownItems}</select>;
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon">
+            <div className="text">Select Color</div>
+            <div className="menu visible transition">{dropdownOptions}</div>
+          </i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
