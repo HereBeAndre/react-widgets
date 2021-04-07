d924d8cc4675883b846808bd871db461f083483f
atotared <atota@red.software.systems>
2021-04-07 22:37:51 +0200

    Add translated term on UI

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,22 +2,33 @@ import React, { useState, useEffect } from "react";
 import googleapi from "../api/googleapi";
 
 const Convert = ({ language, text }) => {
+  const [translatedText, setTranslatedText] = useState("");
   const { value: target } = language;
   useEffect(() => {
-    googleapi.post(
-      "",
-      {}, // Translate API 2nd argument is for body params
-      {
-        params: {
-          q: text,
-          target: target,
-          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await googleapi.post(
+        "",
+        {}, // Translate API 2nd argument is for body params
+        {
+          params: {
+            q: text,
+            target: target,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslatedText(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+    console.log(translatedText);
   }, [target, text]);
 
-  return <div></div>;
+  return (
+    <div>
+      <h3 className="ui header">{translatedText}</h3>
+    </div>
+  );
 };
 
 export default Convert;
